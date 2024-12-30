d08593cee81f192108bb00645169888965866cf5
lfonta2001 <lfontaine2001@gmail.com>
2024-12-30 02:25:32 -0300

    Parte 3 (Producto.cpp) con tests

--- src/producto.cpp
+++ src/producto.cpp
@@ -2,27 +2,43 @@
 
 struct rep_producto {
 
+int id;
+char nombre[MAX_NOMBRE_PRODUCTO];
+int precio;
+TFecha fechaIngreso;
+
 };
 
 
 TProducto crearTProducto(int id, const char nombre[MAX_NOMBRE_PRODUCTO], int precio, TFecha fechaIngreso){
-    return NULL;
+    TProducto nuevoProducto = new(rep_producto);
+    nuevoProducto->id = id;
+    strcpy(nuevoProducto->nombre, nombre);
+    nuevoProducto->precio = precio;
+    nuevoProducto->fechaIngreso = fechaIngreso;
+    return nuevoProducto;
 }
 
 int idTProducto(TProducto producto){
-    return 0;
+    return producto->id;
 }
 
 int precioTProducto(TProducto producto){
-    return 0;
+    return producto->precio;
 }
 
 void imprimirTProducto(TProducto producto){
-
+    printf("Producto: %d\n", producto->id);
+    printf("%s\n", producto->nombre);
+    printf("Precio: %d\n", producto->precio);
+    printf("Ingresado el: ");
+    imprimirTFecha(producto->fechaIngreso);
 }
 
 void liberarTProducto(TProducto &producto){
-
+    liberarTFecha(producto->fechaIngreso);
+    delete producto;
+    producto = NULL;
 }
 
 
