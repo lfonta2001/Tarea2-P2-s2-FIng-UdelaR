72e928c79d6c6c69fb40bee10d66fb9b4bd1340e
lfonta2001 <lfontaine2001@gmail.com>
2025-01-19 17:26:42 -0300

    parte 5 Fin cliente, todos test positivos

--- src/cliente.cpp
+++ src/cliente.cpp
@@ -1,38 +1,50 @@
 #include "../include/cliente.h"
 
 struct rep_cliente {
-    
+    int id;
+    int edad;
+    char nombre[MAX_NOMBRE];
+    char apellido[MAX_APELLIDO];
 };
 
 
 TCliente crearTCliente(int id, const char nombre[MAX_NOMBRE], const char apellido[MAX_APELLIDO], int edad){
-    return NULL;
+    TCliente cliente = new(rep_cliente);
+    cliente->id = id;
+    cliente->edad = edad;
+    strcpy(cliente->nombre, nombre);
+    strcpy(cliente->apellido, apellido);
+    return cliente;
 }
 
 void imprimirTCliente(TCliente cliente){
-
+    printf("Cliente %s %s\n", cliente->nombre, cliente->apellido);
+    printf("Id: %d\n", cliente->id);
+    printf("Edad: %d\n", cliente->edad);
 }
 
 void liberarTCliente(TCliente &cliente){
-
+    delete cliente;
+    cliente = NULL;
 }
 
 void nombreTCliente(TCliente cliente, char nombre[MAX_NOMBRE]){
-
+    strcpy(nombre, cliente->nombre);
 }
 
 void apellidoTCliente(TCliente cliente, char apellido[MAX_APELLIDO]){
-
+    strcpy(apellido, cliente->apellido);
 }
 
 int idTCliente(TCliente cliente){
-    return 0;
+    return cliente->id;
 }
 
 int edadTCliente(TCliente cliente){
-    return 0;
+    return cliente->edad;
 }
 
 TCliente copiarTCliente(TCliente cliente){
-    return NULL;
+    TCliente nuevoCliente = crearTCliente(cliente->id, cliente->nombre, cliente->apellido, cliente->edad);
+    return nuevoCliente;
 }
